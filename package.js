e96db17038d05808ed86d94899fccbda76d8afc5
stefanocudini <stefano.cudini@gmail.com>
2016-04-20 15:42:51 +0200

    fix meteor pkg

--- package.js
+++ package.js
@@ -1,10 +1,11 @@
 Package.describe({
-	version: "0.2.4",
-	name: "bootstrap-list-filter",
-	summary: "bootstrap-list-filter"
+	version: "0.2.5",
+	name: "stefcud:bootstrap-list-filter",
+	summary: "bootstrap-list-filter",
+	git: "https://github.com/stefanocudini/bootstrap-list-filter.git"
 });
 
 Package.on_use(function (api, where) {
 	api.use('bootstrap-3', 'client');
-	api.add_files('bootstrap-list-filter.min.js', 'client');
+	api.addFiles('bootstrap-list-filter.min.js', 'client');
 });
