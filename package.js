5812ad93e3b35a6b48292a37e7d6adb4a51f4991
stefanocudini <stefano.cudini@gmail.com>
2016-04-20 16:29:07 +0200

    update meteor deps

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-	version: "0.2.5",
+	version: "0.2.6",
 	name: "stefcud:bootstrap-list-filter",
 	summary: "bootstrap-list-filter",
 	git: "https://github.com/stefanocudini/bootstrap-list-filter.git"
 });
 
 Package.on_use(function (api, where) {
-	api.use('bootstrap-3', 'client');
+	api.use('twbs:bootstrap', 'client');
 	api.addFiles('bootstrap-list-filter.min.js', 'client');
 });
